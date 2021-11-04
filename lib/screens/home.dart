71f6bac7eb0631e2a6ef12366e0b748529fb0bde
Jorge Mayta <jmayta17@hotmai.com>
2021-11-04 11:48:04 -0500

    add BottomNavigationBar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,11 +1,23 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, must_be_immutable
 
 import 'package:flutter/material.dart';
 import 'package:superhero_app/screens/favorite_list.dart';
 import 'package:superhero_app/screens/super_hero_list.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  var selectedIndex = 0;
+
+  final widgetOptions = [
+    SuperHeroList(),
+    FavoriteList(),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +25,30 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: Text('SuperHero App'),
       ),
-      body: FavoriteList(),
+      body: widgetOptions.elementAt(selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        // ignore: prefer_const_literals_to_create_immutables
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.verified_user),
+            label: 'Superheros',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorites',
+          ),
+        ],
+        currentIndex: selectedIndex,
+        onTap: (index) {
+          onItemTapped(index);
+        },
+      ),
     );
+  }
+
+  void onItemTapped(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
   }
 }
